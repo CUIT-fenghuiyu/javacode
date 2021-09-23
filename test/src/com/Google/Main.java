af7932af55e49c5fdbc2592b346ac374cc552cb2
CUIT-fenghuiyu <2959404472@qq.com>
2021-09-23 22:41:32 +0800

    test

--- test/src/com/Google/Main.java
+++ test/src/com/Google/Main.java
@@ -1,8 +1,20 @@
 package com.Google;
-
+import java.util.Scanner;
 public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
+        public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                double PI=0;
+                double sum=0;
+                for(int i=10000; i<=100000; i+=10000)
+                {
+                        for(int j=1; j<=i; j++)
+                        {
+                                sum += Math.pow(-1,j+1) / (2*j-1);
+                        }
+                        PI = 4*sum;
+                        sum = 0;
+                        System.out.println(PI);
+                }
+                sc.close();
+        }
 }
